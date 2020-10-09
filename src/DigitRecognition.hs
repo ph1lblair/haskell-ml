b3ab844901e026ae2ad45234a3ee25fa1d869254
Phil Blair <ph1lblair@yahoo.co.uk>
2020-10-09 13:51:00 +0100

    DigitRecognition completed

--- src/DigitRecognition.hs
+++ src/DigitRecognition.hs
@@ -7,6 +7,7 @@ import Codec.Compression.GZip (decompress)
 import qualified Data.ByteString.Lazy as BS
 import Data.Functor
 import qualified Foreign.Storable as ForeignStorable
+import GHC.Float (float2Double)
 --import System.Random
 
 render :: Integral a => a -> Char
@@ -21,6 +22,7 @@ render n = let s = " .:oO@" in s !! (fromIntegral n * length s `div` 256)
 --    [render . BS.index s . (n * 28 ^ 2 + 16 + r * 28 +) <$> [0 .. 27] | r <- [0..27]]
 -- print $ BS.index l (n + 8)
 
+clip :: Ord c => c -> c -> c -> c
 clip a b = max a . min b
 
 computeProbabilities :: Matrix R -> Matrix R -> R -> Matrix R
@@ -30,41 +32,85 @@ computeProbabilities x theta temp = cmap (\x -> if x < 1.0e-16 then 0 else x) p 
         p = cmap exp $ (theta LA.<> tr x) / scalar temp - c
         divisor = (rows theta >< rows x) $ concat $ repeat $ map (sum . toList) $ toColumns p
 
-computeCostFunction :: Matrix R -> Vector R -> Matrix R -> R -> R -> R
-computeCostFunction x y theta lambda temp = 3.0
+contributors :: (ForeignStorable.Storable R, Fractional R) => Int -> Int -> Vector Int -> Matrix R
+contributors n k y = (n >< k) $ contributor k y 
+    where
+        contributor k = concatMap (contributor' k) . toList
+        contributor' k v = l ++ [1.0] ++ r
+            where
+                (l, r) = splitAt v $ replicate (k - 1) 0.0
+
+computeCostFunction :: Matrix R -> Vector Int -> Matrix R -> R -> R -> R
+computeCostFunction x y theta lambda temp = loss (rows x) (rows theta) y + regularisation
+    where
+        logProbabilities = cmap log $ cmap (clip 1e-15 (1 - 1e-15)) (computeProbabilities x theta temp)
+        regularisation = lambda / 2 * sumElements (cmap (** 2) theta)
+        loss n k y' = -sumElements (tr (contributors n k y') * logProbabilities) / fromIntegral n
+
+runGradientDescentIteration :: Matrix R -> Vector Int -> Matrix R -> R -> R -> R -> Matrix R
+runGradientDescentIteration x y theta alpha lambda temp = theta - scale alpha grad
     where
         n = rows x
-        d = cols x
         k = rows theta
-        -- contributors = (n >< k) $ repeat 0.0
---        X - (n, d) NumPy array (n datapoints each with d features)
---        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
---        theta - (k, d) NumPy array, where row j represents the parameters of our
---                model for label j
---    for i in range(n):
---        for j in range(k):
---            if Y[i] == j:
---                contributors[i, j] = 1
---    log_probabilities = np.log(np.clip(compute_probabilities(X, theta, temp_parameter), 1e-15, 1-1e-15))
---    loss = -1/n * np.sum(np.transpose(contributors) * log_probabilities)
---    regularisation = 1/2 * lambda_factor * np.sum(np.square(theta))
---    return loss + regularisation
+        m = tr $ contributors n k y
+        probs = cmap (clip 1e-15 (1 - 1e-15)) (computeProbabilities x theta temp)
+        grad = scale lambda theta - (m - probs) LA.<> x / (scalar temp * fromIntegral n)
 
-buildData :: (ForeignStorable.Storable Float, Num Float) => BS.ByteString -> Matrix Float
-buildData ds = (60000 >< (28 ^ 2)) $ map fromIntegral $ BS.unpack $ BS.drop 16 ds
+augmentFeatures :: (Element R, Fractional R) => Matrix R -> Matrix R
+augmentFeatures x = (rows x >< 1) (repeat 1.0) ||| x
+
+softmaxRegression :: Matrix R -> Vector Int -> R -> R -> R -> Int -> Int -> (Matrix R, [R])
+softmaxRegression x y temp alpha lambda k t = iterate step ((k >< cols x') $ repeat 0.0, []) !! t
+    where
+        x' = augmentFeatures x
+        step (theta, cs) =
+            (runGradientDescentIteration x' y theta alpha lambda temp, cs ++ [computeCostFunction x' y theta lambda temp])
+
+getClassifification :: Matrix R -> Matrix R -> R -> [Int]
+getClassifification x theta temp = map maxIndex $ toColumns probs
+    where
+        x' = augmentFeatures x
+        probs = computeProbabilities x' theta temp
+
+computeTestError :: Matrix R -> Vector Int -> Matrix R -> R -> R
+computeTestError x y theta temp = 1 - sum (compare y assignedLabels) / fromIntegral (length assignedLabels)
+    where
+        assignedLabels = getClassifification x theta temp
+        compare y z = zipWith (\x y -> if x == y then 1 else 0) (toList y) z
+
+buildData :: (ForeignStorable.Storable Float, Num Float) => Int -> BS.ByteString -> Matrix R
+buildData length ds = cmap float2Double $ (length >< (28 ^ 2)) $ map ((/ 256) . fromIntegral) $ BS.unpack $ BS.drop 16 ds
 
 buildLabels :: (ForeignStorable.Storable Int, Num Int) => BS.ByteString -> Vector Int
 buildLabels bs = fromList $ map fromIntegral $ BS.unpack $ BS.drop 8 bs
 
 main :: IO ()
 main = do
-    t <- decompress <$> BS.readFile "train-images-idx3-ubyte.gz"
-    l <- decompress <$> BS.readFile "train-labels-idx1-ubyte.gz"
-
-    let trainData = buildData t
-    let trainLabels = buildLabels l
     
-    print $ take 5 $ toList trainLabels
+    tr <- decompress <$> BS.readFile "train-images-idx3-ubyte.gz"
+    trl <- decompress <$> BS.readFile "train-labels-idx1-ubyte.gz"
+    te <- decompress <$> BS.readFile "t10k-images-idx3-ubyte.gz"
+    tel <- decompress <$> BS.readFile "t10k-labels-idx1-ubyte.gz"
+
+    let trainData = buildData 60000 tr
+    let trainLabels = buildLabels trl
+    let testData = buildData 10000 te
+    let testLabels = buildLabels tel
+    let temp = 0.5
+    let alpha = 0.3
+    let lambda = 0.0001
+    let k = 10
+    let iter = 150
+    let (theta, cs) = softmaxRegression trainData trainLabels temp alpha lambda k iter
+--    print $ trainData ? [0]
+--    print $ toList trainLabels !! 0
+--    print $ (computeProbabilities' (augmentFeatures trainData) theta temp) ¿ [0]
+--    let theta' = runGradientDescentIteration (augmentFeatures trainData) trainLabels theta alpha lambda temp
+--    print $ computeCostFunction (augmentFeatures trainData) trainLabels theta lambda temp
+--    print $ theta ¿ [0]
+--    print $ getClassifification testData theta temp
+    print $ computeTestError testData testLabels theta temp
+    -- print $ take 5 $ toList trainLabels
 
 
 
--- test/DigitRecognitionSpec.hs
+++ test/DigitRecognitionSpec.hs
@@ -12,26 +12,51 @@ k :: Int
 k = 7
 x :: Matrix R
 x = (n >< d) $ map fromIntegral [0..(n * d - 1)]
+y :: Vector Int
+y = fromList [0..(n - 1)]
 zeros :: Matrix R
 zeros = (k >< d) $ repeat 0.0
 temp :: R
 temp = 0.2
+lambda :: R
+lambda = 0.5
 
 spec :: Spec
-spec =
-  describe "Compute probabilities" $ do
+spec = do
+    describe "Compute probabilities" $ do
 
-    it "Compute probabilities 1" $ do
-        let
-            zeros = (k >< d) $ repeat 0
-            expected = (k >< n) $ repeat (1.0 / fromIntegral k)
-            result1 = computeProbabilities x zeros temp
-        result1 `shouldBe` expected
+        it "Compute probabilities 1" $ do
+            let
+                expected = (k >< n) $ repeat (1.0 / fromIntegral k)
+                result1 = computeProbabilities x zeros temp
+            result1 `shouldBe` expected
 
-    it "Compute probabilities 2" $ do
-        let
-            theta = (k >< d) $ map fromIntegral [0..(k * d - 1)]
-            temp = 0.2
-            expected = ((k - 1) >< n) (repeat 0.0) === konst 1.0 (1, n)
-            result2 = computeProbabilities x theta temp
-        result2 `shouldBe` expected
+        it "Compute probabilities 2" $ do
+            let
+                theta = (k >< d) $ map fromIntegral [0..(k * d - 1)]
+                temp = 0.2
+                expected = ((k - 1) >< n) (repeat 0.0) === konst 1.0 (1, n)
+                result2 = computeProbabilities x theta temp
+            result2 `shouldBe` expected
+
+    describe "Compute cost" $
+
+        it "Compute cost 1" $ do
+            let
+                cost1 = computeCostFunction x y zeros lambda temp
+            cost1 `shouldBe` 1.9459101490553135
+
+    describe "Gradient descent" $ 
+
+        it "Run gradient descent" $ do
+            let
+                alpha = 2
+                expected = (k >< d) [ -7.142857142857142, -5.238095238095236, -3.333333333333332, -1.4285714285714273, 0.4761904761904778
+                  ,  9.523809523809526, 11.428571428571429, 13.333333333333332, 15.238095238095239, 17.142857142857142
+                  ,  26.19047619047619, 28.095238095238095, 29.999999999999996, 31.90476190476191, 33.80952380952381
+                  , -7.142857142857142,  -8.57142857142857, -10.0, -11.428571428571427, -12.857142857142854
+                  , -7.142857142857141,  -8.57142857142857, -10.0, -11.428571428571427, -12.857142857142854
+                  , -7.142857142857141,  -8.57142857142857, -10.0, -11.428571428571427, -12.857142857142854
+                  , -7.142857142857141,  -8.57142857142857, -10.0, -11.428571428571427, -12.857142857142854 ]
+                m = runGradientDescentIteration x y zeros alpha lambda temp
+            m `shouldBe` expected
